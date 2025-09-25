3553e9ffc73ad18bff1b2a0a9c80ac0f3e4add2b
mathcat4 <zemthct@proton.me>
2025-09-25 13:15:16 +0200

    f u laptop battery

--- project/strophe4.py
+++ project/strophe4.py
@@ -2,4 +2,29 @@ from helpers import *
 
 
 def construct_scene(scene: mn.Scene):
-    pass
+    qm.set_opacity(0.5)
+    scene.add(construction, qm, gm, N, X, S, labelN, labelX, rightS)
+
+    tex1 = mn.MathTex(
+        r"HM(a,b) = \sqrt{\frac{2}{\frac{1}{a} + \frac{1}{b}}}", color=HMCOL
+    ).shift(mn.UP * 3)
+    scene.play(mn.ReplacementTransform(am2, am1))
+    scene.play(mn.ReplacementTransform(am1, dashedam))
+    scene.play(mn.Create(dashed1))
+    scene.play(mn.Create(G), mn.Create(labelG))
+    scene.play(mn.Create(hm), mn.Create(rightG))
+    scene.play(mn.Create(angleX), mn.Create(angleXlabel))
+    scene.play(mn.Wiggle(angXl))
+    scene.play(mn.Wiggle(rightG), mn.Wiggle(rightS))
+    scene.play(mn.Wiggle(AMGMDreieck))
+    scene.play(mn.Wiggle(GMHMDreieck))
+    zauberfunktion(scene, GanzeSkizze, lambda m: m.shift(mn.LEFT * 3))
+    term20 = mn.MathTex(
+        r"\frac{|\overline{XG}|}{|\overline{XS}|} = \frac{|\overline{XS}|}{|\overline{XM}|}",
+        color=TXTCOL,
+    ).move_to(right_half_center)
+
+
+class MainSketch(mn.Scene):
+    def construct(self):
+        construct_scene(self)
