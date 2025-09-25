b429dcec97fd7afdf3d94d5d6611b10eaf91fb2a
Mythendros <0-Anyone@proton.me>
2025-09-25 18:12:53 +0200

    blub

--- project/strophe4.py
+++ project/strophe4.py
@@ -8,6 +8,7 @@ def construct_scene(scene: mn.Scene):
     tex1 = mn.MathTex(
         r"HM(a,b) = \sqrt{\frac{2}{\frac{1}{a} + \frac{1}{b}}}", color=HMCOL
     ).shift(mn.UP * 3)
+    scene.play(mn.Write(tex1))
     scene.play(mn.ReplacementTransform(am2, am1))
     scene.play(mn.ReplacementTransform(am1, dashedam))
     scene.play(mn.Create(dashed1))
@@ -18,6 +19,7 @@ def construct_scene(scene: mn.Scene):
     scene.play(mn.Wiggle(rightG), mn.Wiggle(rightS))
     scene.play(mn.Wiggle(AMGMDreieck))
     scene.play(mn.Wiggle(GMHMDreieck))
+    scene.play(mn.FadeOut(tex1))
     zauberfunktion(scene, GanzeSkizze, lambda m: m.shift(mn.LEFT * 3))
     term20 = mn.MathTex(
         r"\frac{|\overline{XG}|}{|\overline{XS}|} = \frac{|\overline{XS}|}{|\overline{XM}|}",
