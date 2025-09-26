b699126037a256e71a50ac2bb41ad719cd47df30
mathcat4 <zemthct@proton.me>
2025-09-26 10:13:36 +0200

    sync main.py hopefully

--- project/main.py
+++ project/main.py
@@ -12,6 +12,20 @@ def fade_out(scene: mn.Scene, run_time=1):
         scene.play(*anims)
 
 
+refrain1_start = 26.9
+strophe1_start = 44.69
+refrain2_start = 1 * 60 + 5.39
+strophe2_start = 1 * 60 + 22.97
+refrain3_start = 2 * 60 + 7
+strophe3_start = 2 * 60 + 24.97
+refrain4_start = 3 * 60 + 6.5
+strophe4_start = 3 * 60 + 24.04
+refrain5_start = 4 * 60 + 4
+strophe5_start = 4 * 60 + 22.33
+refrain6_start = 5 * 60 + 00.58
+refrain7_start = 5 * 60 + 16.6
+
+
 class MainSketch(mn.Scene):
     def construct(self):
         fade_duration = 1
@@ -31,29 +45,29 @@ class MainSketch(mn.Scene):
         einleitung.construct_scene(self)
 
         # Refrain 1
-        self.wait(26.9 - self.time - fade_duration)
+        self.wait(refrain1_start - self.time - fade_duration)
         fade_out(self, run_time=fade_duration)
         refrain1.construct_scene(self)
 
         # Strophe 1 (AM)
-        self.wait(44.69 - self.time - fade_duration)
+        self.wait(strophe1_start - self.time - fade_duration)
         fade_out(self)
         strophe1.construct_scene(self)
 
         # Refrain 2
-        self.wait(65.39 - self.time - fade_duration)
+        self.wait(refrain2_start - self.time - fade_duration)
         fade_out(self)
         refrain1.construct_scene(self)
 
-        return
+        # return
 
         # Strophe 2 (QM)
-        self.wait(26.9 - self.time)
+        self.wait(strophe2_start - self.time - fade_duration)
         fade_out(self)
         strophe2.construct_scene(self)
 
         # Refrain 3
-        self.wait(26.9 - self.time)
+        self.wait(refrain3_start - self.time - fade_duration)
         fade_out(self)
         refrain1.construct_scene(
             self,
@@ -62,12 +76,12 @@ class MainSketch(mn.Scene):
         )
 
         # Strophe 3 (GM)
-        self.wait(26.9 - self.time)
+        self.wait(strophe3_start - self.time - fade_duration)
         fade_out(self)
         strophe3.construct_scene(self)
 
         # Refrain 4
-        self.wait(26.9 - self.time)
+        self.wait(refrain4_start - self.time - fade_duration)
         fade_out(self)
         refrain1.construct_scene(
             self,
@@ -82,12 +96,12 @@ class MainSketch(mn.Scene):
         )
 
         # Strophe 4 (HM)
-        self.wait(26.9 - self.time)
+        self.wait(strophe4_start - self.time - fade_duration)
         fade_out(self)
         strophe4.construct_scene(self)
 
         # Refrain 5
-        self.wait(26.9 - self.time)
+        self.wait(refrain5_start - self.time - fade_duration)
         fade_out(self)
         refrain1.construct_scene(
             self,
@@ -108,15 +122,15 @@ class MainSketch(mn.Scene):
         )
 
         # Strophe 5 (Ausblick)
-        self.wait(26.9 - self.time)
+        self.wait(strophe5_start - self.time - fade_duration)
         fade_out(self)
         strophe5.construct_scene(self)
 
         # Refrain 6
-        self.wait(26.9 - self.time)
+        self.wait(refrain6_start - self.time - fade_duration)
         fade_out(self)
         refrain6.construct_scene(self)
 
         # Refrain 7
-        self.wait(26.9 - self.time)
+        self.wait(refrain7_start - self.time)
         refrain6.construct_scene(self, reverse=True)
