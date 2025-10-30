e0b149c909a46cb932b1638a45816cda71bb8efa
mathcat4 <zemthct@proton.me>
2025-10-31 00:51:47 +0100

    refactor strophe1

--- project/strophe1.py
+++ project/strophe1.py
@@ -2,8 +2,14 @@ from helpers import *
 
 
 def construct_scene(scene: mn.Scene):
-    """Konstruktion Kreis + AM"""
+    """
+    Animation for Strophe 1. Konstruktion Kreis + AM.
+    """
     geo = Geo()
+
+    ## Part one
+
+    # Initial braces and segments
 
     firsta = mn.Line(
         geo.A.get_center() + (-0.25, 0.5, 0),
@@ -25,8 +31,6 @@ def construct_scene(scene: mn.Scene):
     b2text.set_color(TXTCOL)
     labelAB = mn.VGroup(b1text, b2text)
 
-    ## Part one
-
     scene.play(
         mn.FadeIn(firsta),
         mn.FadeIn(firstb),
@@ -34,6 +38,7 @@ def construct_scene(scene: mn.Scene):
         mn.FadeIn(labelAB),
         mn.FadeIn(b2),
     )
+
     scene.wait(4.13)
     term1 = mn.MathTex(r"a + b", color=TXTCOL).shift(mn.UP * 3)
     scene.play(
@@ -43,6 +48,7 @@ def construct_scene(scene: mn.Scene):
         mn.FadeOut(b2),
         mn.TransformMatchingShapes(labelAB, term1),
     )
+
     term2 = mn.MathTex(r"\frac{a + b}{2}", color=TXTCOL).shift(mn.UP * 3)
     scene.play(
         mn.Create(geo.S),
@@ -51,30 +57,30 @@ def construct_scene(scene: mn.Scene):
         mn.FadeIn(geo.abrtxt),
         mn.FadeIn(geo.bbrtxt),
     )
+
     scene.play(
         mn.Create(geo.M),
         mn.Create(geo.labelM),
         mn.TransformMatchingShapes(term1, term2),
     )
+
     scene.wait(1.74)
     scene.play(mn.Create(geo.semikreis))
-    scene.wait(0.47)
-    term3 = mn.MathTex(r"r =", r"\frac{a + b}{2}", r"= AM(a,b)", color=AMCOL).shift(
+
+    # whole radius moving animation thing
+    scene.wait(1.47)
+    term3 = mn.MathTex("r =", r"\frac{a + b}{2}", "= AM(a,b)", color=AMCOL).shift(
         mn.UP * 3
     )
-
     # bitte bite funktuniertre
-    moving_dot = mn.Dot(geo.semikreis.point_from_proportion(0), color=mn.RED)
-    moving_dot.set_opacity(0)
+    moving_dot = geo.B.copy().set_opacity(0)
     line = mn.always_redraw(
         lambda: mn.Line(geo.M.get_center(), moving_dot.get_center(), color=AMCOL)
     )
     scene.add(moving_dot)
     scene.mobjects.insert(1, line)
     scene.play(
-        term2.animate.move_to(term3[1].get_center()).set_color(
-            AMCOL
-        ),  # morph fraction to new fraction
+        mn.TransformMatchingShapes(term2, term3[1]),  # morph fraction to new fraction
         mn.FadeIn(term3[0]),  # r =
         mn.FadeIn(term3[2]),  # = AM(a,b)
         mn.MoveAlongPath(
@@ -85,15 +91,16 @@ def construct_scene(scene: mn.Scene):
         ),
     )
     scene.remove(line)
-    scene.wait(3)
+
+    scene.wait(2)
     scene.play(mn.Create(geo.labelS))
 
     ## Part two
 
     scene.wait(2.5)
-    scene.play(
-        mn.FadeOut(term2, term3), geo.construction.animate.shift(mn.DOWN + 2 * mn.LEFT)
-    )
+    scene.play(mn.FadeOut(term3), geo.construction.animate.shift(mn.DOWN + 2 * mn.LEFT))
+
+    # Construct chords in semicircle
 
     base_point = lambda val: geo.M.get_center() + [geo.RADIUS * (2 * val - 1), 0, 0]
     arc_point = lambda val: geo.M.get_center() + [
@@ -112,80 +119,82 @@ def construct_scene(scene: mn.Scene):
     scene.play(mn.ReplacementTransform(chord_hm.copy(), chord_qm))
     scene.play(mn.ReplacementTransform(chord_qm.copy(), chord_am))
 
+    # Write mean equations
+
     eq_qm = mn.MathTex("QM(a, b)", color=QMCOL)
     eq_am = mn.MathTex("AM(a, b)", color=AMCOL)
     eq_gm = mn.MathTex("GM(a, b)", color=GMCOL)
     eq_hm = mn.MathTex("HM(a, b)", color=HMCOL)
 
     eq_group = mn.VGroup(eq_qm, eq_am, eq_gm, eq_hm).arrange(mn.DOWN)
-    eq_group.shift(4 * mn.RIGHT + mn.DOWN)
+    eq_group.move_to(RIGHT_HALF_CENTER)
+    eq_group.align_to(geo.semikreis, mn.DOWN)
 
     scene.play(mn.Write(eq_group))
 
-    chord_qm_copy = chord_qm.copy().set_color(QMCOL)
+    # Map mean equations to semicircle
+
+    chord_qm2 = chord_qm.copy().set_color(QMCOL)
     question_chord_qm = mn.Text("?", color=QMCOL).scale(0.5)
     question_chord_qm.next_to(chord_qm.get_center(), mn.DR, buff=0.15)
-    group_chord_qm = mn.VGroup(chord_qm, question_chord_qm)
 
-    chord_am_copy = chord_am.copy().set_color(AMCOL)
+    chord_am2 = chord_am.copy().set_color(AMCOL)
     question_chord_am = mn.Text("?", color=AMCOL).scale(0.5)
     question_chord_am.next_to(chord_am.get_center(), buff=0.15)
-    group_chord_am = mn.VGroup(chord_am, question_chord_am)
 
-    chord_gm_copy = chord_gm.copy().set_color(GMCOL)
+    chord_gm2 = chord_gm.copy().set_color(GMCOL)
     question_chord_gm = mn.Text("?", color=GMCOL).scale(0.5)
     question_chord_gm.next_to(chord_gm.get_center(), mn.LEFT, buff=0.15)
 
-    chord_hm_copy = chord_hm.copy().set_color(HMCOL)
+    chord_hm2 = chord_hm.copy().set_color(HMCOL)
     question_chord_hm = mn.Text("?", color=HMCOL).scale(0.5)
     question_chord_hm.next_to(chord_hm.get_center(), buff=0.15)
 
     scene.play(
-        mn.ReplacementTransform(eq_qm.copy(), chord_qm_copy),
-        mn.ReplacementTransform(eq_am.copy(), chord_am_copy),
-        mn.ReplacementTransform(eq_gm.copy(), chord_gm_copy),
-        mn.ReplacementTransform(eq_hm.copy(), chord_hm_copy),
+        mn.ReplacementTransform(eq_qm.copy(), chord_qm2),
+        mn.ReplacementTransform(eq_am.copy(), chord_am2),
+        mn.ReplacementTransform(eq_gm.copy(), chord_gm2),
+        mn.ReplacementTransform(eq_hm.copy(), chord_hm2),
         mn.FadeIn(
             question_chord_qm, question_chord_am, question_chord_gm, question_chord_hm
         ),
         run_time=1,
     )
     scene.bring_to_front(geo.construction)
-    scene.remove(chord_qm_copy, chord_am_copy, chord_gm_copy, chord_hm_copy)
+    scene.remove(chord_qm, chord_am, chord_gm, chord_hm)
 
-    chord_qm.set_color(QMCOL)
-    chord_am.set_color(AMCOL)
-    chord_gm.set_color(GMCOL)
-    chord_hm.set_color(HMCOL)
+    # Draw copies of AM and QM
+
+    group_chord_am2 = mn.VGroup(chord_am2, question_chord_am)
+    group_chord_qm2 = mn.VGroup(chord_qm2, question_chord_qm)
+
+    chord_am3 = chord_am2.copy().rotate(-chord_am2.get_angle())
+    question_chord_am3 = question_chord_am.copy().next_to(
+        chord_am3.get_center(), mn.UP, buff=0.15
+    )
+    group_chord_am3 = mn.VGroup(chord_am3, question_chord_am3).move_to(
+        3.5 * mn.UP + 1.7 * mn.LEFT
+    )
+
+    chord_qm3 = chord_qm2.copy().rotate(-chord_qm2.get_angle())
+    question_chord_qm3 = question_chord_qm.copy().next_to(
+        chord_qm3.get_center(), mn.DOWN, buff=0.2
+    )
+    group_chord_qm3 = (
+        mn.VGroup(chord_qm3, question_chord_qm3)
+        .next_to(group_chord_am3, mn.DOWN, buff=1.5)
+        .align_to(group_chord_am3, mn.LEFT)
+    )
 
     scene.wait(1)
+    scene.play(mn.ReplacementTransform(group_chord_am2, group_chord_am3))
+    scene.play(mn.ReplacementTransform(group_chord_qm2, group_chord_qm3))
 
-    chord_am2 = chord_am.copy().rotate(-chord_am.get_angle())
-    question_chord_am2 = question_chord_am.copy().next_to(
-        chord_am2.get_center(), mn.UP, buff=0.15
-    )
-    group_chord_am2 = mn.VGroup(chord_am2, question_chord_am2).move_to(
-        3.5 * mn.UP + 2 * mn.LEFT
-    )
-
-    chord_qm2 = chord_qm.copy().rotate(-chord_qm.get_angle())
-    question_chord_qm2 = question_chord_qm.copy().next_to(
-        chord_qm2.get_center(), mn.DOWN, buff=0.2
-    )
-    group_chord_qm2 = (
-        mn.VGroup(chord_qm2, question_chord_qm2)
-        .next_to(group_chord_am2, mn.DOWN, buff=1.5)
-        .align_to(group_chord_am2, mn.LEFT)
-    )
-
-    scene.play(mn.ReplacementTransform(group_chord_am.copy(), group_chord_am2))
-    scene.play(mn.ReplacementTransform(group_chord_qm.copy(), group_chord_qm2))
-
-    scene.wait(1)
+    # Relation and mean anims
 
     eq_rel = mn.MathTex(r"\leq", color=TXTCOL).rotate(-mn.PI / 2)
     eq_rel.move_to((chord_am2.get_center() + chord_qm2.get_center()) / 2)
-
+    scene.wait(1)
     scene.play(mn.Write(eq_rel))
 
     eq_qm_copy = mn.MathTex("QM(a, b)?", color=QMCOL).next_to(chord_qm2, buff=1)
@@ -194,12 +203,9 @@ def construct_scene(scene: mn.Scene):
         .next_to(chord_am2)
         .set_x(eq_qm_copy.get_x())
     )
-
     eq_rel_copy = eq_rel.copy()
     eq_rel_copy.move_to((eq_am_copy.get_center() + eq_qm_copy.get_center()) / 2)
-
     scene.wait(2)
-
     scene.play(
         mn.ReplacementTransform(chord_am2.copy(), eq_am_copy),
         mn.ReplacementTransform(eq_rel.copy(), eq_rel_copy),
@@ -207,8 +213,11 @@ def construct_scene(scene: mn.Scene):
     )
 
     scene.wait(3)
-
     fadeout_all(scene)
+
+    ## Part 3
+
+    # Triangle setup
 
     point_C = mn.Dot(mn.ORIGIN, color=TXTCOL)
     point_A = mn.Dot(3 * mn.RIGHT, color=TXTCOL)
@@ -230,10 +239,21 @@ def construct_scene(scene: mn.Scene):
         dot_color=RIGHTANGLECOL,
         color=RIGHTANGLECOL,
     )
+    scene.bring_to_back(rightangle)
 
     label_a = mn.MathTex("k_1", color=TXTCOL).next_to(side_BC, mn.LEFT)
     label_b = mn.MathTex("k_2", color=TXTCOL).next_to(side_CA, mn.DOWN)
     label_c = mn.MathTex("h", color=TXTCOL).next_to(side_AB.get_center(), mn.UR)
+
+    scene.play(
+        mn.FadeIn(triangle),
+        mn.FadeIn(rightangle),
+        mn.FadeIn(label_a),
+        mn.FadeIn(label_b),
+        mn.FadeIn(label_c),
+    )
+
+    # Write theorem and equation step by step
 
     eq_thm = (
         mn.Paragraph(
@@ -247,21 +267,20 @@ def construct_scene(scene: mn.Scene):
         .shift(3 * mn.UP)
     )
 
-    scene.bring_to_back(rightangle)
-    scene.play(
-        mn.FadeIn(triangle),
-        mn.FadeIn(rightangle),
-        mn.FadeIn(label_a),
-        mn.FadeIn(label_b),
-        mn.FadeIn(label_c),
-    )
-
-    scene.wait(0.5)
     scene.play(
         mn.Write(typ.cast(mn.VMobject, eq_thm.chars[0])),
     )
 
-    scene.play(mn.Write(typ.cast(mn.VMobject, eq_thm.chars[1])))
+    eq_ineq1 = mn.MathTex(r"h \geq k_1", color=TXTCOL)
+    eq_ineq2 = mn.MathTex(r"h \geq k_2", color=TXTCOL)
+    group_ineq = mn.VGroup(eq_ineq1, eq_ineq2).arrange(mn.DOWN)
+    group_ineq.move_to(RIGHT_HALF_CENTER)
+
+    scene.play(
+        mn.Write(typ.cast(mn.VMobject, eq_thm.chars[1])),
+        mn.Write(group_ineq),
+        mn.Create(mn.SurroundingRectangle(group_ineq, color=TXTCOL)),
+    )
 
 
 class MainSketch(mn.Scene):
