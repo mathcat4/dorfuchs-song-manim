4678e3a3fb8f8ceea312755f43ff4523c53b117a
mathcat4 <zemthct@proton.me>
2025-11-02 23:44:46 +0100

    should be final refactor

--- project/strophe2.py
+++ project/strophe2.py
@@ -2,74 +2,28 @@ from helpers import *
 
 
 def construct_scene(scene: mn.Scene):
-    """Konstruktion QM"""
+    """
+    Animation for strophe 2. Konstruktion QM.
+    """
+
     geo = Geo()
+    scene.add(geo.construction)
 
-    term1 = (
-        mn.MathTex(r"{{{a+b \over 2}}}", color=TXTCOL)
-        .move_to(RIGHT_HALF_CENTER)
-        .shift(mn.LEFT * 1.5)
+    tex1 = mn.MathTex(r"QM(a,b) = \sqrt{\frac{a^2+b^2}{2}}", color=QMCOL).shift(
+        mn.UP * 3
     )
-    term2 = (
-        mn.MathTex(r"{a+b \over 2} - a", color=TXTCOL)
-        .move_to(RIGHT_HALF_CENTER)
-        .shift(mn.RIGHT * 1.5)
-    )
-    term3 = (
-        mn.MathTex(r"{{{b-a \over 2}}}", color=TXTCOL)
-        .move_to(RIGHT_HALF_CENTER)
-        .shift(mn.RIGHT * 1.5)
-    )
-    term4 = (
-        mn.MathTex(
-            r"{{|\overline{SN}|}}{{^2}} {{=}} {{(\kern0pt}}{{{a+b \over 2}}}{{)\kern0pt}}{{^2\kern0pt}} {{+}} {{(}}{{{b-a \over 2}}}{{)}}{{\hspace{0pt}^2}} ",
-            color=TXTCOL,
-        )
-        .move_to(RIGHT_HALF_CENTER)
-        .shift(mn.UP * 1.5)
-    )
-    term5 = (
-        mn.MathTex(
-            r"{{|\overline{SN}|}}{{^2}} {{=}} {  {{(\kern0pt}}a+b{{)\kern0pt}} {{^2\kern0pt}}  {{\over}} {{2^2}} } {{+}} { {{(}}b-a{{)}} {{\hspace{0pt}^2}} {{\over}} {{2^2}} } ",
-            color=TXTCOL,
-        )
-        .move_to(RIGHT_HALF_CENTER)
-        .shift(mn.UP * 1.5)
-    )
-    term6 = (
-        mn.MathTex(
-            r"{{|\overline{SN}|}}{{^2}} {{=}} {  {{(\kern0pt}}a+b{{)\kern0pt}} {{^2\kern0pt}} {{+}} {{(}}b-a{{)}} {{\hspace{0pt}^2}}  {{\over}} {{2^2}}}",
-            color=TXTCOL,
-        )
-        .move_to(RIGHT_HALF_CENTER)
-        .shift(mn.UP * 1.5)
-    )
-    term7 = mn.MathTex(
-        r"{{|\overline{SN}|}}{{^2}} {{=}} {{{a^2}} + 2ab + {{b^2}} {{+}} {{a^2}} - 2ab + {{b^2}} \over {{2^2}}}",
-        color=TXTCOL,
-        font_size=36,
-    ).move_to(RIGHT_HALF_CENTER)
-    term8 = mn.MathTex(
-        r"{{|\overline{SN}|}}{{^2}} {{=}} {2{{a^2}} {{+}} 2{{b^2}} {{\over}} {{2^2}}}",
-        color=TXTCOL,
-    ).move_to(RIGHT_HALF_CENTER)
-    term9 = mn.MathTex(
-        r"{{|\overline{SN}|}}{{^2}} {{=}}  { {{a^2}} {{+}} {{b^2}} {{\over}} {{2}} } ",
-        color=TXTCOL,
-    ).move_to(RIGHT_HALF_CENTER)
-    term10 = (
-        mn.MathTex(
-            r"{{|\overline{SN}|}} {{=}}  {{ \sqrt{a^2 + b^2 \over 2} }}",
-            color=TXTCOL,
-        )
-        .move_to(RIGHT_HALF_CENTER)
-        .shift(mn.DOWN * 1.5)
-    )
+    scene.wait(1)
+    scene.play(mn.Write(tex1), run_time=2)
 
-    term11 = mn.MathTex(
-        r"{{|\overline{SN}|}} {{=}} {{ \sqrt{a^2 + b^2 \over 2} }} = QM(a,b)",
-        color=QMCOL,
-    ).move_to(RIGHT_HALF_CENTER)
+    scene.wait(6.27)
+    scene.bring_to_back(geo.am2)
+    scene.play(mn.FadeIn(geo.am2), mn.FadeIn(geo.N), run_time=1)
+
+    scene.wait(1.88)
+    scene.bring_to_back(geo.qm)
+    scene.play(mn.Create(geo.qm), run_time=1)
+    scene.play(mn.Create(geo.labelN), run_time=1)
+    scene.play(mn.Create(geo.rightM), run_time=1)
 
     bquestion = mn.Brace(
         geo.qm,
@@ -78,75 +32,121 @@ def construct_scene(scene: mn.Scene):
     )
     bqtext = bquestion.get_tex("?")
     bqtext.set_color(TXTCOL)
+    scene.play(mn.FadeIn(bquestion, bqtext), run_time=0.75)
+    scene.play(mn.FadeOut(bquestion, bqtext), run_time=0.75)
 
-    # ANIMATION START 1:22,97
-
-    scene.add(geo.construction)
-    # vorbereitung
-    scene.add(geo.lineMS)
-    tex1 = mn.MathTex(r"QM(a,b) = \sqrt{\frac{a^2+b^2}{2}}", color=QMCOL).shift(
-        mn.UP * 3
-    )
-    scene.play(mn.Write(tex1), run_time=2)
-    # 24,97
-    scene.wait(7.27)
-    # 32,24
-    scene.bring_to_back(geo.am2)
-    scene.play(mn.FadeIn(geo.am2), mn.FadeIn(geo.N), run_time=1)
-    scene.wait(1.88)
-    # 35,12
-    scene.bring_to_back(geo.qm)
-    scene.play(mn.Create(geo.qm), run_time=1)
-    scene.play(mn.Create(geo.labelN), run_time=1)
-    scene.play(mn.Create(geo.rightM), run_time=1)
-    scene.play(mn.FadeIn(bquestion, bqtext), run_time=0.5)
-    scene.wait(0.5)
-    scene.play(mn.FadeOut(bquestion, bqtext), run_time=0.5)
     scene.wait(1)
-    # 40,61
     scene.play(mn.Wiggle(geo.QMAMDreieck), run_time=1.7)
     scene.add(
         geo.am2
     )  # sieht zwar fett dumm aus aber is nötig, weil das anwenden von Gruppen Daten komisch umformatiert (und man das iwie wieder auflösen muss) und deswegen meine hex hex funktion nd funktioniert.
     scene.bring_to_back(geo.am2)
+
     scene.play(mn.FadeOut(tex1, run_time=0.5))
     zauberfunktion(scene, geo.GanzeSkizze, lambda m: m.shift(mn.LEFT * 3))
+
     scene.wait(0.25)
-    # 44,06
     scene.play(mn.Wiggle(geo.am2), run_time=1.7)
     am2_copy = geo.am2.copy()
+    term1 = (
+        mn.MathTex(r"{{{a+b \over 2}}}", color=TXTCOL)
+        .move_to(RIGHT_HALF_CENTER)
+        .shift(mn.LEFT * 1.5)
+    )
     scene.play(
         mn.ReplacementTransform(am2_copy, term1),
         run_time=1,
     )
+
     scene.wait(1.02)
-    # 47,78
-    scene.remove(geo.lineMS)
-    geo.lineMS.color = mn.RED
-    scene.mobjects.insert(4, geo.am3)
-    scene.play(mn.FadeIn(geo.am3), run_time=0.4)
+    scene.bring_to_front(geo.am3)
     scene.bring_to_front(geo.S)
     scene.bring_to_front(geo.M)
-    scene.play(mn.ReplacementTransform(geo.am3, geo.lineMS), run_time=1.2)
+    scene.play(mn.FadeIn(geo.am3), run_time=0.4)
+
+    scene.play(
+        mn.ReplacementTransform(geo.am3, geo.lineMS.set_color(mn.RED)),
+        run_time=1.2,
+    )
     scene.play(mn.Wiggle(geo.lineMS, scale_value=1.5), run_time=1.3)
-    scene.play(mn.ReplacementTransform(geo.lineMS, term2), run_time=0.76)
+
+    term2 = (
+        mn.MathTex(r"{a+b \over 2} - a", color=TXTCOL)
+        .move_to(RIGHT_HALF_CENTER)
+        .shift(mn.RIGHT * 1.5)
+    )
+    scene.play(mn.Transform(geo.lineMS, term2), run_time=0.76)
+    scene.remove(geo.lineMS)
+
+    # Pure eqution manipulation
+
+    term3 = (
+        mn.MathTex(r"{{{b-a \over 2}}}", color=TXTCOL)
+        .move_to(RIGHT_HALF_CENTER)
+        .shift(mn.RIGHT * 1.5)
+    )
     scene.play(mn.TransformMatchingShapes(term2, term3), run_time=0.5)
 
-    # scene.play(geo.lineMS.animate.set_color(TXTCOL), run_time=0.14)
-    # 51,86
+    term4 = (
+        mn.MathTex(
+            r"{{|\overline{SN}|}}{{^2}} {{=}} {{(\kern0pt}}{{{a+b \over 2}}}{{)\kern0pt}}{{^2\kern0pt}} {{+}} {{(}}{{{b-a \over 2}}}{{)}}{{\hspace{0pt}^2}} ",
+            color=TXTCOL,
+        )
+        .move_to(RIGHT_HALF_CENTER)
+        .shift(mn.UP * 1.5)
+    )
     scene.play(mn.TransformMatchingTex(mn.Group(term3, term1), term4), run_time=1)
+
+    term5 = (
+        mn.MathTex(
+            r"{{|\overline{SN}|}}{{^2}} {{=}} {  {{(\kern0pt}}a+b{{)\kern0pt}} {{^2\kern0pt}}  {{\over}} {{2^2}} } {{+}} { {{(}}b-a{{)}} {{\hspace{0pt}^2}} {{\over}} {{2^2}} } ",
+            color=TXTCOL,
+        )
+        .move_to(RIGHT_HALF_CENTER)
+        .shift(mn.UP * 1.5)
+    )
     scene.wait(0.03)
     scene.play(mn.TransformMatchingTex(term4, term5), run_time=1)
+
+    term6 = (
+        mn.MathTex(
+            r"{{|\overline{SN}|}}{{^2}} {{=}} {  {{(\kern0pt}}a+b{{)\kern0pt}} {{^2\kern0pt}} {{+}} {{(}}b-a{{)}} {{\hspace{0pt}^2}}  {{\over}} {{2^2}}}",
+            color=TXTCOL,
+        )
+        .move_to(RIGHT_HALF_CENTER)
+        .shift(mn.UP * 1.5)
+    )
     scene.wait(0.03)
     scene.play(mn.TransformMatchingTex(term5, term6), run_time=1)
+
+    term7 = mn.MathTex(
+        r"{{|\overline{SN}|}}{{^2}} {{=}} {{{a^2}} + 2ab + {{b^2}} {{+}} {{a^2}} - 2ab + {{b^2}} \over {{2^2}}}",
+        color=TXTCOL,
+        font_size=36,
+    ).move_to(RIGHT_HALF_CENTER)
     scene.wait(0.03)
-    # 54,95
     scene.play(TransformMatchingTexNoReplace(term6, term7), run_time=1)
+
+    term8 = mn.MathTex(
+        r"{{|\overline{SN}|}}{{^2}} {{=}} {2{{a^2}} {{+}} 2{{b^2}} {{\over}} {{2^2}}}",
+        color=TXTCOL,
+    ).move_to(RIGHT_HALF_CENTER)
     scene.play(mn.TransformMatchingTex(term7, term8), run_time=1)
+
+    term9 = mn.MathTex(
+        r"{{|\overline{SN}|}}{{^2}} {{=}}  { {{a^2}} {{+}} {{b^2}} {{\over}} {{2}} } ",
+        color=TXTCOL,
+    ).move_to(RIGHT_HALF_CENTER)
     scene.play(mn.TransformMatchingTex(term8, term9), run_time=1)
-    # alt
-    # scene.add(mn.index_labels(term17))
-    # scene.add(mn.index_labels(term18))
+
+    term10 = (
+        mn.MathTex(
+            r"{{|\overline{SN}|}} {{=}}  {{ \sqrt{a^2 + b^2 \over 2} }}",
+            color=TXTCOL,
+        )
+        .move_to(RIGHT_HALF_CENTER)
+        .shift(mn.DOWN * 1.5)
+    )
     scene.play(
         mn.TransformMatchingShapes(term9[0].copy(), term10[0]),
         mn.TransformMatchingShapes(term9[3].copy(), term10[2]),
@@ -163,13 +163,16 @@ def construct_scene(scene: mn.Scene):
         run_time=1,
     )
 
+    term11 = mn.MathTex(
+        r"{{|\overline{SN}|}} {{=}} {{ \sqrt{a^2 + b^2 \over 2} }} = QM(a,b)",
+        color=QMCOL,
+    ).move_to(RIGHT_HALF_CENTER)
     scene.play(
         mn.TransformMatchingTex(term10, term11),
         mn.FadeOut(term9),
         mn.FadeOut(term6),
         run_time=1,
     )
-    # 59.95
 
 
 class MainSketch(mn.Scene):
--- project/strophe3.py
+++ project/strophe3.py
@@ -2,39 +2,44 @@ from helpers import *
 
 
 def construct_scene(scene: mn.Scene):
-    """Konstruktion GM"""
+    """
+    Animation for strophe 3. Konstruktion GM.
+    """
     geo = Geo()
-
     scene.add(geo.qm, geo.construction, geo.N)
 
-    term4 = mn.MathTex(r"a \cdot b", color=TXTCOL).shift(mn.UP * 3)
-    term5 = mn.MathTex(r"\sqrt{ab}", color=TXTCOL).shift(mn.UP * 3)
-    term6 = mn.MathTex(r"GM(a,b) =", r"\sqrt{ab}", color=GMCOL).shift(mn.UP * 3)
-    # ANIMATION START 2:24,97
+    # Equation manipulation
+
+    term1 = mn.MathTex(r"a \cdot b", color=TXTCOL).shift(mn.UP * 3)
     scene.wait(1.77)
-    # 26:74
-    scene.play(mn.Write(term4), run_time=1.5)
+    scene.play(mn.Write(term1), run_time=1.5)
+
+    term2 = mn.MathTex(r"\sqrt{ab}", color=TXTCOL).shift(mn.UP * 3)
     scene.wait(1.62)
-    # 29,86
-    scene.play(mn.TransformMatchingShapes(term4, term5), run_time=1)
+    scene.play(mn.TransformMatchingShapes(term1, term2), run_time=1)
+
+    term3 = mn.MathTex(r"GM(a,b) =", r"\sqrt{ab}", color=GMCOL).shift(mn.UP * 3)
     scene.play(
-        mn.FadeIn(term6[0]),
-        term5.animate.move_to(term6[1].get_center()).set_color(GMCOL),
+        mn.FadeIn(term3[0]),
+        term2.animate.move_to(term3[1].get_center()).set_color(GMCOL),
         run_time=1.5,
     )
+
     scene.wait(4.7)
-    # 37,06
-    scene.play(mn.FadeOut(term6), mn.FadeOut(term5), run_time=1)
+    scene.play(mn.FadeOut(term3), mn.FadeOut(term2), run_time=1)
+
+    # Geometric construction
+
     scene.wait(6.14)
-    # 44,2
     scene.mobjects.insert(1, geo.rightS)
     scene.mobjects.insert(1, geo.gm)
     scene.play(mn.Create(geo.gm), mn.Create(geo.rightS), run_time=1)
+
     scene.wait(2.48)
-    # 47,68
     scene.play(mn.Create(geo.X), run_time=1)
     scene.play(mn.Create(geo.labelX), run_time=1)
     scene.mobjects.insert(1, geo.am1)
+
     lineAX = mn.Line(geo.A.get_center(), geo.X.get_center(), color=TXTCOL)
     lineBX = mn.Line(geo.B.get_center(), geo.X.get_center(), color=TXTCOL)
     scene.play(
@@ -43,44 +48,47 @@ def construct_scene(scene: mn.Scene):
         mn.Create(geo.am1),
         run_time=1,
     )
+
     scene.wait(1.17)
-    # 51,85
     scene.mobjects.insert(4, geo.rightX)  # viel hacky aber klappt lmao
     scene.play(mn.Create(geo.rightX), run_time=1)
-    scene.wait(2.72)
-    # 55,57
-    term7 = mn.MathTex(
+
+    # Write equation and wiggle wiggle
+
+    term4 = mn.MathTex(
         r"|\overline{SX}|", r"^2", r"=", r"a", r"\cdot", r"b", color=TXTCOL
     ).shift(mn.UP * 3)
-
-    # ZEITEN ÄNDERN WENN MICHAEL RAP FIXT
+    scene.wait(2.72)
     scene.play(mn.Wiggle(geo.gm), run_time=0.5)
-    scene.play(mn.ReplacementTransform(geo.gm.copy(), term7[0]), run_time=0.5)
+    scene.play(mn.ReplacementTransform(geo.gm.copy(), term4[0]), run_time=0.5)
+
     scene.play(mn.Wiggle(geo.gm), run_time=0.5)
     scene.play(
-        mn.ReplacementTransform(geo.gm.copy(), term7[1]),
-        mn.Write(typ.cast(mn.VMobject, term7[2])),
+        mn.ReplacementTransform(geo.gm.copy(), term4[1]),
+        mn.Write(typ.cast(mn.VMobject, term4[2])),
         run_time=0.5,
     )
+
     scene.play(mn.Wiggle(geo.sega), run_time=0.5)
-    scene.play(mn.ReplacementTransform(geo.sega.copy(), term7[3]), run_time=0.5)
+    scene.play(mn.ReplacementTransform(geo.sega.copy(), term4[3]), run_time=0.5)
+
     scene.play(mn.Wiggle(geo.segb), run_time=0.5)
     scene.play(
-        mn.ReplacementTransform(geo.segb.copy(), term7[5]),
-        mn.Write(typ.cast(mn.VMobject, term7[4])),
+        mn.ReplacementTransform(geo.segb.copy(), term4[5]),
+        mn.Write(typ.cast(mn.VMobject, term4[4])),
         run_time=0.5,
     )
 
-    scene.wait(0.1)
-
-    term8 = mn.MathTex(
+    term5 = mn.MathTex(
         r"{{|\overline{SX}|}}{{=}}\sqrt{{{a}}{{b}}}", color=TXTCOL
     ).shift(mn.UP * 3)
-    term9 = mn.MathTex(r"|\overline{SX}| = \sqrt{ab}", r"= GM(a,b)", color=GMCOL).shift(
+    scene.wait(0.1)
+    scene.play(mn.TransformMatchingTex(term4, term5))
+
+    term6 = mn.MathTex(r"|\overline{SX}| = \sqrt{ab}", r"= GM(a,b)", color=GMCOL).shift(
         mn.UP * 3
     )
-    scene.play(mn.TransformMatchingTex(term7, term8))
-    scene.play(mn.FadeIn(term9[1]), term8.animate.move_to(term9[0]).set_color(GMCOL))
+    scene.play(mn.FadeIn(term6[1]), term5.animate.move_to(term6[0]).set_color(GMCOL))
 
 
 class MainSketch(mn.Scene):
